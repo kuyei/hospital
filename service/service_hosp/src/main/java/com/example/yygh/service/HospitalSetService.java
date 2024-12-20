e5782f0ad18990274f05cddb8c18f3ff67faf024
chenxi <yuchenxi08@gmail.com>
2024-12-20 16:41:44 +0800

    Initial commit

--- service/service_hosp/src/main/java/com/example/yygh/service/HospitalSetService.java
+++ service/service_hosp/src/main/java/com/example/yygh/service/HospitalSetService.java
@@ -1,7 +1,7 @@
 package com.example.yygh.service;
 
-import com.example.yygh.model.hosp.HospitalSet;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.yygh.model.hosp.HospitalSet;
 
 
 public interface HospitalSetService extends IService<HospitalSet> {
